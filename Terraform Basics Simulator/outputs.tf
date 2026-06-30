055b4593da69d91c66a17eb27f9a2d84a5c5e14e
Cloud Engineer <sktamilvb@gmail.com>
2026-06-30 16:23:49 +0530

    Update outputs.tf

--- Terraform Basics Simulator/outputs.tf
+++ Terraform Basics Simulator/outputs.tf
@@ -2,10 +2,10 @@ output "instance_id" {
   value = aws_instance.web.id
 }
 
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+output "public_ip" {
+  value = aws_eip.web.public_ip
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.assets.bucket
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
